969d422adef98ce59cf618bf90e20c88a9f4cb90
Rodrigo Luger <rodluger@gmail.com>
2018-04-23 14:35:04 -0300

    added dummy tests

---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+"""Test the code."""
+from starry import Star, Planet, System
+import numpy as np
+
+
+def test_main():
+    """Test the code."""
+    # Nothing for now!
+    pass
+
+
+if __name__ == "__main__":
+    test_main()
