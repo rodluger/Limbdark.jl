1191f4d5d244f146a31beae8ce756a84c26a1824
Rodrigo Luger <rodluger@gmail.com>
2018-04-23 17:49:11 -0300

    Update build-paper.sh

--- .ci/build-paper.sh
+++ .ci/build-paper.sh
@@ -27,7 +27,7 @@ then
     # Force push the paper to GitHub
     cd $TRAVIS_BUILD_DIR
     git checkout --orphan $TRAVIS_BRANCH-pdf
-    git rm -rf .
+    #git rm -rf .
     git add -f tex/limbdark.pdf
     git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
     git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
