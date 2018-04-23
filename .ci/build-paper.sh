6a293ca417ece9b9e0f3e824e17a3e96d77b7228
Rodrigo Luger <rodluger@gmail.com>
2018-04-23 17:17:28 -0300

    trying to get travis to work

--- .ci/build-paper.sh
+++ .ci/build-paper.sh
@@ -10,15 +10,14 @@ then
 
     # Generate the figures
     echo "Generating figures..."
-    cd tex/figures
+    cd $TRAVIS_BUILD_DIR/tex/figures
     for f in *.py; do
         echo "Running $f..."
         python "$f"
     done
-    cd ../../
 
     # Build the paper
-    cd tex/
+    cd $TRAVIS_BUILD_DIR/tex
 	pdflatex -interaction=nonstopmode -halt-on-error limbdark.tex
 	bibtex limbdark
 	pdflatex -interaction=nonstopmode -halt-on-error limbdark.tex
@@ -28,7 +27,7 @@ then
     # Force push the paper to GitHub
     cd $TRAVIS_BUILD_DIR
     git checkout --orphan $TRAVIS_BRANCH-pdf
-    git rm -rf . > /dev/null 2>&1
+    git rm -rf .
     git add -f tex/limbdark.pdf
     git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
     git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
