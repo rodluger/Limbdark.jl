ea2174384dec23fa34c50ba3a74acdd527af1541
Rodrigo Luger <rodluger@gmail.com>
2018-05-25 10:31:46 -0700

    placeholders for figures

---
+++ tex/figures/s2_residuals.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as pl
+fig, ax = pl.figure()
+ax.set_title("Placeholder for s2_residuals.pdf")
+ax.axis('off')
+fig.savefig("s2_residuals.pdf", bbox_inches='tight')
---
+++ tex/figures/transit_linear.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as pl
+fig, ax = pl.figure()
+ax.set_title("Placeholder for transit_linear.pdf")
+ax.axis('off')
+fig.savefig("transit_linear.pdf", bbox_inches='tight')
