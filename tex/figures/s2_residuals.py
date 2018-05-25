14a6fbb555b5e553a208e124994f26eab0cff8db
Rodrigo Luger <rodluger@gmail.com>
2018-05-25 11:57:44 -0700

    duh

--- tex/figures/s2_residuals.py
+++ tex/figures/s2_residuals.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as pl
-fig, ax = pl.figure()
+fig, ax = pl.subplots(1)
 ax.set_title("Placeholder for s2_residuals.pdf")
 ax.axis('off')
 fig.savefig("s2_residuals.pdf", bbox_inches='tight')
