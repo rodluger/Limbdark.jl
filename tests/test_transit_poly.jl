4646b88d6c3caee11fa53236762ac4d374f3b74f
Eric Agol <agol@uw.edu>
2018-10-03 16:32:45 -0700

    Adding lightcurve test for 30 limb-darkening components to test_transit_poly.jl

--- tests/test_transit_poly.jl
+++ tests/test_transit_poly.jl
@@ -8,30 +8,34 @@ include("../src/transit_poly_struct.jl")
 
 # Now, integrate by hand:
 function transit_poly_int(r,b,u)
-s_1 = maximum([0.0,b-r]); s_2=minimum([1.0,b+r])
-ns = 10000; ds=(s_2-s_1)/ns
-s = linspace(s_1+.5*ds,s_2-.5*ds,ns)
-fobs = zero(r)
-for j=1:ns
-  if s[j] < r-b
-    dphi = 2pi
-  else
-    dphi = 2*acos((s[j]^2+b^2-r^2)/(2*b*s[j]))
+if b < (1.0+r)
+  s_1 = maximum([0.0,b-r]); s_2=minimum([1.0,b+r])
+  ns = 10000; ds=(s_2-s_1)/ns
+  s = linspace(s_1+.5*ds,s_2-.5*ds,ns)
+  fobs = zero(r)
+  for j=1:ns
+    if s[j] < r-b
+      dphi = 2pi
+    else
+      dphi = 2*acos((s[j]^2+b^2-r^2)/(2*b*s[j]))
+    end
+    z = sqrt(1-s[j]^2)
+    imu = 1.0
+    for n=1:n
+      imu -= u[n]*(1-z)^n
+    end
+    fobs += s[j]*ds*dphi*imu
   end
-  z = sqrt(1-s[j]^2)
-  imu = 1.0
+  norm = 1.
   for n=1:n
-    imu -= u[n]*(1-z)^n
+    norm -= 2*u[n]/(2+3*n+n^2)
   end
-  fobs += s[j]*ds*dphi*imu
+  fobs /= pi*norm
+  fobs = 1-fobs
+  return fobs
+else
+  return 1.0
 end
-norm = 1.
-for n=1:n
-  norm -= 2*u[n]/(2+3*n+n^2)
-end
-fobs /= pi*norm
-fobs = 1-fobs
-return fobs
 end
 
 r0=[0.01,0.01,0.01,0.01,0.01,0.1,0.1,0.1,0.1,0.1,1.0,1.0,1.0,10.,10.,10.,100.,100.,100.]
@@ -67,5 +71,31 @@ for i=1:length(r0)
   @test isapprox(f_num,flux,atol=1e-5)
   println("r: ",r," b: ",b," f_an: ",flux," f_num: ",f_num," diff: ",flux-f_num)
 end
+
+# Now, compute light curves and plot:
+
+npts = 1000
+iu = 30
+u_n = ones(iu) / iu
+r = 0.1
+b0 = zeros(npts)
+for i=1:npts
+    b0[i] = 3.0 * (i / float(npts) - 0.5)
+end
+
+lc_ana = zeros(npts); lc_num=zeros(npts)
+for i=1:length(b0)
+  b=b0[i]
+  if mod(i,10) == 0
+    println("i: ",i,"b: ",b)
+  end
+  lc_ana[i] = transit_poly(r,abs(b),u_n)
+  lc_num[i] = transit_poly_int(r,abs(b),u_n)
+end
+
+using PyPlot
+
+plot(b0,lc_ana, linewidth=2, label="Analytic")
+plot(b0,lc_num, linewidth=1, label="Numeric")
 
 end
