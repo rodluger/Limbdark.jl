45c4ca2435abe3da0a2ddbded9ec19d7b6a90f54
Rodrigo Luger <rodluger@gmail.com>
2018-10-10 15:50:54 -0400

    travis issues

--- .ci/build-paper.sh
+++ .ci/build-paper.sh
@@ -11,7 +11,8 @@ then
     cd $TRAVIS_BUILD_DIR/tex/figures/julia
     for f in *.jl; do
         echo "Running $f..."
-        LD_PRELOAD=${HOME}/.julia/v0.7/Conda/deps/usr/lib/libz.so julia "$f"
+        #LD_PRELOAD=${HOME}/.julia/v0.7/Conda/deps/usr/lib/libz.so julia "$f"
+        julia "$f"
     done
 
     # Generate the Python figures
