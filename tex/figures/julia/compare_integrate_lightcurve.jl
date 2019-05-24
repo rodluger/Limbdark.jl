52a4e128ff63502505c523b9dbdfcbdb99bf4b8b
Ian Williams <ianw89@live.com>
2019-05-24 15:10:36 -0700

    Got PyCall working, preparing to call exoplanet's time integration code

--- tex/figures/julia/compare_integrate_lightcurve.jl
+++ tex/figures/julia/compare_integrate_lightcurve.jl
@@ -2,18 +2,17 @@
 
 using BenchmarkTools
 using PyPlot
+using PyCall
 
-if VERSION >= v"0.7"
-  using Statistics
-  using DelimitedFiles
-end
+# Python modules used for comparison
+exo = pyimport("exoplanet")
+tt = pyimport("theano.tensor")
+starry = pyimport("starry")
 
 include("../../../src/integrate_lightcurve.jl")
 include("../../../test/loglinspace.jl")
 
 println("\nRunning comparison...\n")
-
-# TODO read in data from starry or exoplanet for comparison
 
 function run_ALFM_test(r::Ty, b0::Ty, aonr::Ty, period::Ty, u_1::Ty, u_2::Ty, trials::Int64) where {Ty <: Real}
 
@@ -49,40 +48,56 @@ function run_ALFM_test(r::Ty, b0::Ty, aonr::Ty, period::Ty, u_1::Ty, u_2::Ty, tr
   return nobs, timeTaken/trials
 end
 
+function run_exoplanet_test(orbit::PyObject, trials::Int64)
+
+  u = tt.vector()
+  b = tt.vector()
+  r = tt.vector()
+  #lc = StarryLightCurve(orbit=)
+
+
+end
+
 
 # Transit parameters. Some vary, some are fixed
 
 # Ratio of semi-major axis to stellar radius:
+# TODO may need to compute aonr?
 aonr = 30.0
 # Orbital period [d]:
-period_vals = collect(linearspace(1, 1000.0, 20)) # = 4.28746668
-#r = 0.025717 # radius ratio
+period_vals = collect(logarithmspace(0.4, 4.0, 25)) # from about 2.5 to about 10000 days
 r = 0.1
-b_vals = [0.3]
-#b_vals = collect(linearspace(0.1, 0.4, 4)) # impact parameter
-#b[i+1] = sqrt(((i-500.0)/500.0*(1.0+2.0*r))^2) # use i-1
+b = 0.3
+
 # Quadratic limb darkening parameters
 u_1 = 0.2
 u_2 = 0.2
 
+
 # Comparison configuration
-runs = length(period_vals) * length(b_vals) # number of parameter configurations we're trying
+runs = length(period_vals)
 trialsPerConfig = 10 # number of trials to run for a given parameter configuration
 
 # Structures for holding results
 num_datapoints = zeros(runs)
 limbdark_time = zeros(runs)
-#results_aveflux = zeros(runs)
+exoplanet_time = zeros(runs)
 
 # Run the timing test
 for i = 1:runs
-  b = b_vals[mod(i-1, length(b_vals)) + 1]
   period = period_vals[mod(i-1, length(period_vals)) + 1]
+
+  # with these parameters, it will assume solar mass and solar radius
+  # TODO that probably isn't consistent with what we did above
+  orbit = exo.orbits.KeplerianOrbit(period=period, b=b)
 
   # the number of data points is computed based on the parameters provided
   # this is the most interesting thing to graph
+  # TODO use orbit data. mine is probably not a real orbit
   num_datapoints[i], limbdark_time[i] = run_ALFM_test(r, b, aonr, period, u_1, u_2, trialsPerConfig)
-  # TODO run starry/exoplanet here
+
+  #exoplanet_time[i] = run_exoplanet_test(orbit, trialsPerConfig)
+
 end
 
 # Plot Results
@@ -93,36 +108,15 @@ ax.plot(num_datapoints, limbdark_time, label = "ALFM (2019)", linestyle = "-", l
 ax.set_title("r=0.1; b=0.3")
 ax.set_xlabel("Data Points")
 ax.set_ylabel("Time [s]")
-#ax.set_xscale(:log) # scales lineary to number of data points
+ax.set_xscale(:log) # scales lineary to number of data points
+ax.set_yscale(:log) # scales lineary to number of data points
 ax.legend(loc="upper left",fontsize=10)
 
-
-################
-# SECOND TEST - VARY B
-################
-
-# TODO not that interesting as is since it affects the number of data points
-# which we already did above... this is less obvious
-
-b_vals = collect(linearspace(0.0, 1.1, 50))
-runs = length(b_vals)
-junk = zeros(runs)
-limbdark_b_time = zeros(runs)
-trialsPerConfig = 30
-
-# Run the timing test
-for i = 1:runs
-  b = b_vals[i]
-  period = 50.0
-
-  junk[i], limbdark_b_time[i] = run_ALFM_test(r, b, aonr, period, u_1, u_2, trialsPerConfig)
-  # TODO run starry/exoplanet here
-end
-
-
+#=
 ax = axes[2]
 ax.plot(b_vals, limbdark_b_time, label = "ALFM (2019)", linestyle = "-", lw=2, color="C0")
 ax.set_title("r=0.1; period=50d")
 ax.set_xlabel("Impact Parameter")
 ax.set_ylabel("Time [s]")
 ax.legend(loc="upper left",fontsize=10)
+=#
