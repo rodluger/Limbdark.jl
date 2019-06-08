7a309ac7cc53a514574b58b5ca7857f48dce55a9
Ian Williams <ianw89@live.com>
2019-06-07 23:07:55 -0700

    Prepare for checkin

--- tex/figures/julia/compare_integrate_lightcurve.jl
+++ tex/figures/julia/compare_integrate_lightcurve.jl
@@ -21,22 +21,13 @@ topplot = axes[1]
 middleplot = axes[2]
 perfplot = axes[3]
 
-# Duration is observed duration in hours
-# Returns a time array in units of hours
-# function get_time_points(duration::Real)
-#   # Simulate transit data with a 2-minute cadence:
-#   tobs = 3 * duration  # Observation duration in hours
-#   nobs = convert(Int64,round(tobs * 30))  # Number of data points
-#   return linearspace(-1.5*duration, 1.5*duration, nobs)  # Time array in units of hours
-# end
-
 function get_time_array(dataPoints::Real)
   #trans_dur = 2*sqrt((1+r)^2-b0^2)/v*24 # approx transit duration in hours
   return linearspace(0.0 - 0.5*get_duration_days(), 0.5*get_duration_days(), dataPoints)
 end
 
 function get_duration_days()
-  return 3.0 # TODO hardcoded. Should vary with the orbital paramters. Fine for this one-off test.
+  return 2.0
 end
 
 function get_exposure_hours(dataPoints::Real)
@@ -46,20 +37,16 @@ end
 
 function run_ALFM_test(r::Float64, b0::Float64, s::Float64, period::Float64, u_1::Float64, u_2::Float64, dataPoints::Int64, trials::Int64, graph::PyObject=nothing)
 
-  #aonr = a / r # change units of semi-major axis from solar radii to planet radii
-  #v = 2*pi*aonr/period # velocity estimate
-  speed = s / r / 24 # change units of speed to be in terms of planet radii instead of solar radii, and hours instead of days
-
   time = get_time_array(dataPoints)
 
   expression, timeTaken, bytes, gctime, memallocs = @timed for k=1:trials
 
     # Setup all the parameters
     trans = transit_init(r, b0, [u_1, u_2], false)  # Initialize transit structure
-    param = [0.0, speed, b0]   # parameters of the transit: [t_0,v,b_0]
+    param = [0.0, s, b0]   # parameters of the transit: [t_0, v, b_0]
     t = Array{Float64,1}(undef,dataPoints)
     t .= time
-    dt = get_exposure_hours(dataPoints)  #2/60  # Transit exposure time is 2 minutes, convert to hours.
+    dt = get_exposure_hours(dataPoints)
     favg1 = Array{Float64,2}(undef, 7, dataPoints)
     tol = 1e-6
     maxdepth = 6
@@ -69,7 +56,7 @@ function run_ALFM_test(r::Float64, b0::Float64, s::Float64, period::Float64, u_1
     integrate_lightcurve!(trans, param, t, dt, favg1, dataPoints, tol, maxdepth, neval_t, depthmax)
 
     if (graph != nothing && k==1)
-      graph.plot(t, favg1[1,:], label="ALFM (n=$dataPoints)", linestyle = "-", lw=2)
+      graph.plot(t, favg1[1,:], label="ALFM (n=$dataPoints)", linestyle = "--", lw=2)
     end
   end
 
@@ -87,19 +74,17 @@ function run_exoplanet_test(orbit::PyObject, r::Ty, u_1::Ty, u_2::Ty, dataPoints
 
     if (graph != nothing && k==1)
       lc = lc.+1.0 # move relative flux to be on same scale as ALFM
-      graph.plot(t, lc, label="exoplanet (n=$dataPoints)", linestyle = "-", lw=2)
+      graph.plot(t, lc, label="exoplanet (n=$dataPoints)", linestyle = "-", lw=1)
     end
   end
 
   return timeTaken/trials
 end
 
-# Transit parameters. Some vary, some are fixed
-
-datapoints_vals = collect(logarithmspace(1.0, 6.0, 15)) # TODO more values and higher values for final comparison
-#period_vals = collect(logarithmspace(0.4, 3.0, 25)) # orbital period (~2.5 to 10000 days)
-period = 4000.0 # days
-r = 0.08 # radius of planet in units of stellar radius TODO varying this produces unexpected results from limbdark?
+# Transit parameters
+datapoints_vals = collect(logarithmspace(1.0, 7.0, 15))
+period = 1000.0 # days
+r = 0.1 # radius of planet in units of stellar radius TODO varying this produces unexpected results from limbdark?
 b = 0.2 # impact parameter
 
 # Quadratic limb darkening parameters
@@ -107,7 +92,7 @@ u_1 = 0.2; u_2 = 0.2
 
 # Comparison configuration
 runs = length(datapoints_vals)
-trialsPerConfig = 10 # number of trials to run for a given parameter configuration
+trialsPerConfig = 5 # number of trials to run for a given parameter configuration
 
 # Structures for holding results
 num_datapoints = zeros(runs)
@@ -123,13 +108,16 @@ v0 = orbit.get_planet_velocity(0.0)
 s0 = sqrt((v0[1].eval()[1]^2.0) + (v0[2].eval()[1]^2.0) + (v0[3].eval()[1]^2.0))
 #println("  Speed: $s0 solar_radii / day")
 
+# which iteration to graph the light curves (higher is more data points)
+runToGraph = 7
+
 # Run the timing test
 for i = 1:runs
   #period = period_vals[mod(i-1, length(period_vals)) + 1]
   dataPoints = convert(Int64, round(datapoints_vals[i]))
 
   graph_to_use::PyObject = nothing
-  if (6 <= i <= 6)
+  if (i == runToGraph)
     graph_to_use = topplot
   end
 
@@ -140,10 +128,11 @@ end
 
 # extra test for different period, for debugging
 r2 = 0.02
-dataPoints = convert(Int64, round(datapoints_vals[6]))
+dataPoints = convert(Int64, round(datapoints_vals[runToGraph]))
 run_ALFM_test(r2, b, s0, period, u_1, u_2, dataPoints, trialsPerConfig, middleplot)
 run_exoplanet_test(orbit, r2, u_1, u_2, dataPoints, trialsPerConfig, middleplot)
 
+# Prepare labels for the light curve comparisons (for accuracy debugging)
 topplot.set_title("Light Curves (r=$r; b=$b; T=$period d)")
 topplot.set_xlabel("Time [days]")
 topplot.set_ylabel("Relative Flux")
--- tex/figures/python/compare_integrate_lightcurve.py
+++ tex/figures/python/compare_integrate_lightcurve.py
@@ -1,118 +1,45 @@
 """Time integration comparison tests"""
+# TODO - this is a work in progress. It's purpose is to see if calling python
+# from Julia has overhead that isn't present when running that code directly
 import exoplanet as exo
-#from julia import Base, Limbdark
-#sys.path.insert(0, os.path.join(os.getenv("HOME", "~"), "starry"))
-#import starry
-#import time
 import matplotlib.pyplot as pl
 import numpy as np
-#import subprocess
-
-# Marker size is proportional to log error
-def ms(error):
-    return 18 + np.log10(error)
 
 # Input params
-u1 = 0.4
-u2 = 0.26
-mstar = 1       # solar masses
-rstar = 1       # solar radii
+u_1 = 0.2
+u_2 = 0.2
 rplanet = 0.1   # fraction of stellar radius
-b0 = 0.5        # impact parameter
-P = 50          # orbital period in days
-
-# Compute the semi-major axis from Kepler's
-# third law in units of rstar (for batman)
-a = ((P * 86400) ** 2 * (1.32712440018e20 * mstar) /
-     (4 * np.pi ** 2)) ** (1. / 3.) / (6.957e8 * rstar)
-
-
-# Get the inclination in degrees
-inc = np.arccos(b0 / a) * 180 / np.pi
-
-# Call Exoplanet's time integrate of the lightcurve
-
-
-
+b0 = 0.2        # impact parameter
+P = 1000          # orbital period in days
+texp = 2.0 / 60.0
 
 # Timing params
-number = 10
-nN = 8
-Nmax = 5
-Narr = np.array(np.logspace(1, Nmax, nN), dtype=int)
-agol_time = np.zeros(nN)
-starry_time = np.zeros(nN)
+#nN = 8
+#Nmax = 5
+#Narr = np.array(np.logspace(1, Nmax, nN), dtype=int)
+runs = 1
 
-#Limbdark.integrate_lightcurve()
-
-
-# Loop over number of cadences
-for i, N in enumerate(Narr):
-
-    # Time array
-    t = np.linspace(-0.15, 0.15, N)
-
-    # Use exoplanet to compute b(t)
-    orbit = exo.orbits.KeplerianOrbit(period=P, b=b0, a=a)
-    coords = orbit.get_relative_position(t)
-    x = coords[0].eval()
-    y = coords[1].eval()
-    b = np.sqrt(x * x + y * y)
-
-
-
-    # Feed b(t) to julia
-    # HACK: PyJulia is currently broken, so this is how we have to do this...
-    # np.savetxt("b.txt", X=b)
-    # np.savetxt("u.txt", X=[u1, u2])
-    # foo = subprocess.check_output(['julia', "compare_to_batman.jl"])
-    # agol_time[i] = float(foo.decode('utf-8'))
-    # agol_flux = np.loadtxt("flux.txt")
-    # foo = subprocess.check_output(['julia', "compare_to_batman_grad.jl"])
-    # agol_grad_time[i] = float(foo.decode('utf-8'))
-
-    # starry
-    # map = starry.Map(ydeg=0, udeg=2)
-    # map[1:] = [u1, u2]
-    # tstart = time.time()
-    # for k in range(number):
-    #     starry_flux = map.flux(b=b, ro=0.1)
-    # starry_time[i] = (time.time() - tstart) / number
-    # tstart = time.time()
-    # for k in range(number):
-    #     map.flux(b=b, ro=0.1, bf=np.ones_like(b))
-    # starry_grad_time[i] = (time.time() - tstart) / number
-
-
+num_datapoints = np.zeros(runs)
+limbdark_time = np.zeros(runs)
+exoplanet_time = np.zeros(runs)
 
 # Plot
-# fig = pl.figure(figsize=(7, 4))
-# ax = pl.subplot2grid((2, 5), (0, 0), colspan=4, rowspan=2)
-# axleg1 = pl.subplot2grid((2, 5), (0, 4))
-# axleg1.axis('off')
-#
-# ax.plot(Narr, agol_time, 'o', ms=ms(err_agol), color='C0')
-# ax.plot(Narr, agol_time, '-', lw=0.75, color='C0')
-# ax.plot(Narr, agol_grad_time, 'o', ms=ms(err_agol), color='C0')
-# ax.plot(Narr, agol_grad_time, '--', lw=0.75, color='C0')
-# ax.plot(Narr, starry_time, 'o', ms=ms(err_starry), color='C2')
-# ax.plot(Narr, starry_time, '-', lw=0.75, color='C2')
-# ax.plot(Narr, starry_grad_time, 'o', ms=ms(err_starry), color='C2')
-# ax.plot(Narr, starry_grad_time, '--', lw=0.75, color='C2')
-#
-# # Tweak and save
-# ax.set_ylabel("Time [s]", fontsize=10)
-# ax.set_xlabel("Number of points", fontsize=10)
-# #ax.set_xscale('log')
-# #ax.set_yscale('log')
-#
-# # Legend
-# axleg1.plot([0, 1], [0, 1], color='C0', label='ALFM (2019)', lw=1.5)
-# axleg1.plot([0, 1], [0, 1], color='C2', label='starry', lw=1.5)
-# axleg1.set_xlim(2, 3)
-# leg = axleg1.legend(loc='center', frameon=False, fontsize=8)
-# leg.set_title('method', prop={'weight': 'bold'})
-# leg.set_title('log error', prop={'weight': 'bold'})
+fig = pl.figure(figsize=(7, 4))
+ax = pl.subplot2grid((2, 5), (0, 0), colspan=4, rowspan=2)
+axleg1 = pl.subplot2grid((2, 5), (0, 4))
+axleg1.axis('off')
 
+# Loop over number of cadences
+for i in range(runs):
 
-#fig.savefig("compare_to_batman.pdf", bbox_inches='tight')
+    dataPoints = 1000
+    t = np.linspace(-1.5, 1.5, dataPoints)
+
+    orbit = exo.orbits.KeplerianOrbit(period=P, b=b0, ecc=0.0, omega=0.0)
+
+    lc = exo.StarryLightCurve([u_1, u_2]).get_light_curve(orbit=orbit, r=rplanet, t=t, texp=texp, order=2).eval()
+
+    ax.plot(t, lc, label="exoplanet (n=$dataPoints)", linestyle = "-", lw=1)
+
+# TODO get plotting working
+pl.show(fig)
