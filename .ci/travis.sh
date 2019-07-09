9b8261c2c6776c38a4f0d2aa69e806a58cebe883
Dan F-M <foreman.mackey@gmail.com>
2019-07-09 11:54:42 -0400

    installing texlive in a separate script

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -61,21 +61,3 @@ cat > $HOME/.config/Tectonic/config.toml << EOL
 [[default_bundles]]
 url = "http://purl.org/net/pkgwpub/tectonic-default"
 EOL
-
-# EXPERIMENTAL: Also installing texlive
-# to resolve issues w/ Julia & LaTeX
-texlive_year="2018"
-sudo apt-get -qq update
-export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
-if ! command -v pdflatex > /dev/null; then
-    echo "Texlive not installed"
-    echo "Downloading texlive and installing"
-    wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
-    tar -xzf install-tl-unx.tar.gz
-    ./install-tl-*/install-tl --profile=./utilities/texlive.profile
-    echo "Finished install TexLive"
-fi
-echo "Now updating TexLive"
-tlmgr option -- autobackup 0
-tlmgr update --self --all --no-auto-install
-echo "Finished updating TexLive"
